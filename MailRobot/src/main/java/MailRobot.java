d556ff391f1bcdbf0b013da17f0c7b164df71e18
zutt <yannlederrey@gmail.com>
2018-04-16 12:54:44 +0200

    adding SMTP sending, need to be formalize with object mail

--- MailRobot/src/main/java/MailRobot.java
+++ MailRobot/src/main/java/MailRobot.java
@@ -1,4 +1,20 @@
-package main.java;
+
+import smtp.SmtpClient;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class MailRobot {
+
+    final static Logger LOG = Logger.getLogger(MailRobot.class.getName());
+
+    public static void main(String[] args) {
+        SmtpClient smtp = new SmtpClient("localhost", 2525);
+    }
 }
